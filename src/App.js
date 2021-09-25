0ba8d4ac00042066907f7fad885d1f7387ff3bea
Aron <75965557+jagdlied@users.noreply.github.com>
2021-09-25 15:28:53 +0800

    Logo speed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> jabol.
+          i like to jabol -sun tzu.
         </p>
         <a
           className="App-link"
