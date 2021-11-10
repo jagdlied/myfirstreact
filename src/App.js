97cc9fc51f0a694c474e534f55e91464b1898c73
Aron <75965557+jagdlied@users.noreply.github.com>
2021-11-10 15:24:30 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          i like to jabol -sun tzu.
+          i like to jabol everyday -sun tzu.
         </p>
         <br>
         <button type="button">Click Me!</button>
